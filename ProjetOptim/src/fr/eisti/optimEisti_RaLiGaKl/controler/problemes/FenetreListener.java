d9bcc8e317be93649a460e350ee39c484a50cc47
razavetmae@eisti.eu <razavetmae@eisti.eu@d6681d19-dd96-8a83-2376-7179d2861502>
2011-05-12 12:49:31 +0000

    début du code de lexport vers excel avec tests N'est pas fonctionnelle encore

--- ProjetOptim/src/fr/eisti/optimEisti_RaLiGaKl/controler/problemes/FenetreListener.java
+++ ProjetOptim/src/fr/eisti/optimEisti_RaLiGaKl/controler/problemes/FenetreListener.java
@@ -2,6 +2,7 @@ package fr.eisti.optimEisti_RaLiGaKl.controler.problemes;
 
 import fr.eisti.optimEisti_RaLiGaKl.model.BDDUtilisateur;
 import fr.eisti.optimEisti_RaLiGaKl.model.BddProbleme;
+import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentEvent;
@@ -18,10 +19,13 @@ import javax.swing.event.ChangeListener;
 import fr.eisti.optimEisti_RaLiGaKl.view.problemes.Fenetre;
 import fr.eisti.optimEisti_RaLiGaKl.Main;
 import fr.eisti.optimEisti_RaLiGaKl.model.Contrainte;
+import fr.eisti.optimEisti_RaLiGaKl.model.FiltreSimple;
 import fr.eisti.optimEisti_RaLiGaKl.model.Utilitaire;
 import fr.eisti.optimEisti_RaLiGaKl.view.problemes.PanelProbleme;
 import java.awt.event.ComponentListener;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import javax.swing.JFileChooser;
 
@@ -50,9 +54,8 @@ public class FenetreListener implements ChangeListener, ActionListener, Componen
      * Constructeur du listener de la fenetre sans parametre
      */
     public FenetreListener() {
-       this(null);
+        this(null);
     }
-
 
     @Override
     public void stateChanged(ChangeEvent e) {
@@ -74,14 +77,14 @@ public class FenetreListener implements ChangeListener, ActionListener, Componen
             }
         }
 
-         //on supprime tous les composants du panel contenant la fonction objectif avec tous les panels
+        //on supprime tous les composants du panel contenant la fonction objectif avec tous les panels
         ((PanelProbleme) Main.fenetrePrincipale.getDroite().getSelectedComponent()).getPanDonnees().removeAll();
 
         
         ((PanelProbleme) Main.fenetrePrincipale.getDroite().getSelectedComponent()).getPanDonnees().add(new JLabel("y="));
-         JTextField jtff = new JTextField(3);
+        JTextField jtff = new JTextField(3);
         ((PanelProbleme) Main.fenetrePrincipale.getDroite().getSelectedComponent()).getPanDonnees().add(jtff);
-          jtff.addKeyListener(new SaveListener());
+        jtff.addKeyListener(new SaveListener());
         ((PanelProbleme) Main.fenetrePrincipale.getDroite().getSelectedComponent()).getPanDonnees().add(new JLabel("x0"));
 
         for (int i = 1; i < source.getValue(); i++) {
@@ -120,15 +123,57 @@ public class FenetreListener implements ChangeListener, ActionListener, Componen
             this.fenetre.dispose();
         } else if (e.getSource() == this.fenetre.getQuitter()) {
             System.exit(0);
-       
-          
+
+
         } else if (e.getSource() == this.fenetre.getAffResHtml()) {
             try {
-                Utilitaire.creerHTML("bdd/"+BDDUtilisateur.getNomUtilisateur() + ".xml", "HTML/resultats.xsl", "HTML/"+BDDUtilisateur.getNomUtilisateur() + ".html");
+                /*JFileChooser fc = new JFileChooser();
+                fc.addChoosableFileFilter(new FiltreSimple("Fichier HTML", ".html"));
+                fc.setAcceptAllFileFilterUsed(false);
+                int returnVal = fc.showSaveDialog(null);
+                if (returnVal == JFileChooser.APPROVE_OPTION) {
+                    String nom = fc.getSelectedFile().getAbsolutePath();
+                    String nom2 = new File(nom).getParent();
+                    if (!nom.endsWith(".html")) {
+                        nom = nom + ".html";
+                    }*/
+                    // Utilitaire.creerHTML("bdd/" + BDDUtilisateur.getNomUtilisateur() + ".xml", "HTML/resultats.xsl", nom);
+                 Utilitaire.creerHTML("bdd/" + BDDUtilisateur.getNomUtilisateur() + ".xml", "HTML/resultats.xsl", BDDUtilisateur.getNomUtilisateur() + ".html");
+                     //prendre en compte les html
+                    /*String inputList[];
+                    File inputDirectory = new File("HTML");
+                    File outputDirectory = new File(nom2+"dossier");
+                    outputDirectory.mkdir();
+                    inputList = inputDirectory.list();
+                    for (int i = 0; i < inputList.length; i++) {
+                        File inputFile = new File(inputDirectory, inputList[i]);
+                        if (!inputFile.isDirectory()) {
+                            try {
+                                File outputFile = new File(outputDirectory, inputList[i]);
+                                outputFile.createNewFile();
+
+                                FileReader in = new FileReader(inputList[i]);
+                                FileWriter out = new FileWriter(outputFile);
+                                int c;
+
+                                while ((c = in.read()) != -1) {
+                                    out.write(c);
+                                }
+
+                                in.close();
+                                out.close();
+                            } catch (IOException e1) {
+                                System.out.println(e1);
+                            }
+                        }
+
+                    }*/
+                //}
+               
             } catch (Exception ex) {
                 Logger.getLogger(FenetreListener.class.getName()).log(Level.SEVERE, null, ex);
             }
-        }else if (e.getSource() == this.fenetre.getRecharger()) {
+        } else if (e.getSource() == this.fenetre.getRecharger()) {
             try {
                 BddProbleme.load(BDDUtilisateur.getNomUtilisateur(), BDDUtilisateur.getImage());
                 Main.fenetrePrincipale.getGauche().miseajour();
@@ -148,7 +193,6 @@ public class FenetreListener implements ChangeListener, ActionListener, Componen
             this.fenetre.setExtendedState(Fenetre.MAXIMIZED_BOTH);
         } else if (e.getSource() == this.fenetre.getAffResHtml()) {
             //ouvrir la page html
-            
         }
     }
 
