a338c85fb00d81e9de9d13d146554b7c0a9d3764
lioncedric@eisti.eu <lioncedric@eisti.eu@d6681d19-dd96-8a83-2376-7179d2861502>
2011-05-13 14:59:00 +0000

    -affichage du resulta operationnel -bugs secondaires corriges

---
+++ ProjetOptim/src/fr/eisti/optimEisti_RaLiGaKl/view/problemes/BoutonHautBas1.java
@@ -0,0 +1,57 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.eisti.optimEisti_RaLiGaKl.view.problemes;
+
+import fr.eisti.optimEisti_RaLiGaKl.model.Basculer;
+import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Administrator
+ */
+public class BoutonHautBas1 extends JPanel implements MouseListener {
+
+    private boolean position;
+    private String NonImageHaut;
+    private String NonImageBas;
+    private String NonImageSelect;
+    private PanelProbleme panelProbleme;
+
+    public BoutonHautBas1(PanelProbleme panelProbleme) {
+        position = false;
+        this.setBackground(Color.red);
+        this.addMouseListener(this);
+        this.panelProbleme = panelProbleme;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        if (panelProbleme.getHauteur() == 0 || panelProbleme.getHauteur() == -270) {
+            position = !position;
+            Thread basculer = new Thread(new Basculer(position));
+
+            basculer.start();
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+    }
+}
