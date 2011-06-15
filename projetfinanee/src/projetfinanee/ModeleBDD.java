1446410409a8446ef50c038a2957435b148d6123
lioncedric@eisti.eu <lioncedric@eisti.eu@d6681d19-dd96-8a83-2376-7179d2861502>
2011-06-15 10:08:50 +0000

    vesion tres dangereuse jar en construction html peu bugger scilab bugge

--- projetfinanee/src/projetfinanee/ModeleBDD.java
+++ projetfinanee/src/projetfinanee/ModeleBDD.java
@@ -1,14 +1,44 @@
-
-
 package projetfinanee;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ModeleBDD {
 
-    public static void load(GrapheValue gr){
-
+    public static void loadDriver() throws ClassNotFoundException {
+        Class.forName("com.mysql.jdbc.Driver");
     }
 
-    public static void save(GrapheValue gr){
- 
+    public static Connection newConnection() throws SQLException {
+        final String url = "jdbc:mysql://localhost/dbessai";
+        Connection conn = DriverManager.getConnection(url, "bduser", "SECRET");
+        return conn;
+    }
+
+    public static void load(GrapheValue gr) throws SQLException {
+        Connection conn = null;
+        try {
+            // create new connection and statement
+            conn = newConnection();
+            Statement st = conn.createStatement();
+
+            String query = "SELECT nom,prenom,age FROM personne ORDER BY age";
+            ResultSet rs = st.executeQuery(query);
+
+            while (rs.next()) {
+                System.out.printf("%-20s | %-20s | %3d\n", rs.getString(1), rs.getString("prenom"), rs.getInt(3));
+            }
+        } finally {
+            // close result, statement and connection
+            if (conn != null) {
+                conn.close();
+            }
+        }
+    }
+
+    public static void save(GrapheValue gr) {
     }
 }
