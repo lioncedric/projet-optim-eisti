ca99b5c3b3b743e9affb23f494b2f06d8d49324c
lioncedric@eisti.eu <lioncedric@eisti.eu@d6681d19-dd96-8a83-2376-7179d2861502>
2011-06-15 14:35:37 +0000

    vesion tres dangereuse jar en construction html peu bugger scilab bugge

--- projetfinanee/src/projetfinanee/ModeleBDD.java
+++ projetfinanee/src/projetfinanee/ModeleBDD.java
@@ -7,25 +7,36 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class ModeleBDD {
 
     public static void load(GrapheValue gr) throws SQLException, ClassNotFoundException {
         List<Personne> pers = new ArrayList<Personne>();
+         Set<AreteValuee> ar = new HashSet<AreteValuee>();
         Connection conn = null;
         try {
             // create new connection and statement
             MyConnector.setParametersOracleLocal("projet", "projet");
             conn = MyConnector.getConnection();
+            //creation des personnes
             Statement st = conn.createStatement();
             String query = "SELECT id_personne,nom,prenom,s.libelle  FROM personne p, sexe s where p.id_sexe=s.id_sexe";
             ResultSet rs = st.executeQuery(query);
-
             while (rs.next()) {
                 System.out.print(rs.getInt(1) + rs.getString(2) + rs.getString(3) + rs.getString(4) + "\n");
                 Personne p = new Personne(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), new HashSet<CentreInteret>(),new HashSet<Sejour>());
+                pers.add(p);
             }
-
+            // //creation des liens
+            st = conn.createStatement();
+            query = "SELECT id_personne,nom,prenom,s.libelle  FROM personne p, sexe s where p.id_sexe=s.id_sexe";
+            rs = st.executeQuery(query);
+            while (rs.next()) {
+                System.out.print(rs.getInt(1) + rs.getString(2) + rs.getString(3) + rs.getString(4) + "\n");
+                Personne p = new Personne(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), new HashSet<CentreInteret>(),new HashSet<Sejour>());
+                pers.add(p);
+            }
         } finally {
             // close result, statement and connection
             if (conn != null) {
